006dcb83d6ce30260ccd0dc2fdef18451c8ba58f
savioacp <savioacp@gmail.com>
2020-10-07 16:52:16 -0300

    Updated Middleware.cs
    Updated middleware to match EF on the project.

---
+++ Bookworm API/JWTMiddleware.cs
@@ -0,0 +1,85 @@
+﻿using Bookworm_API.Models;
+using Microsoft.Ajax.Utilities;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Bookworm_API.Middlewares
+{
+	public class JWTMiddleware : DelegatingHandler
+	{
+		protected async override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+		{
+			var _ = (request, cancellationToken);
+
+			var authHeader = request.Headers.Authorization;
+
+			if (authHeader == null)
+				return Next(_);
+
+			if (authHeader.Scheme != "Bearer")
+				return Next(_);
+
+			if(string.IsNullOrWhiteSpace(authHeader.Parameter))
+				return Next(_);
+
+			var tokenHandler = new JwtSecurityTokenHandler();
+
+			if (!tokenHandler.CanReadToken(authHeader.Parameter))
+				return request.CreateErrorResponse(HttpStatusCode.BadRequest, "Malformed Token");
+
+			var claimsIdentity = tokenHandler.ValidateToken(authHeader.Parameter, new Microsoft.IdentityModel.Tokens.TokenValidationParameters() {
+				ValidateAudience = false,
+				ValidateIssuer = false,
+				RequireExpirationTime = false,
+				IssuerSigningKey = new SymmetricSecurityKey(Convert.FromBase64String(WebApiApplication.JWTSecret))
+			}, out var token).Identity as ClaimsIdentity;
+			
+			if(claimsIdentity == null)
+				return request.CreateErrorResponse(HttpStatusCode.BadRequest, "Empty Claims");
+
+			UserKind kind;
+			try
+			{
+				kind = (UserKind)Enum.Parse(typeof(UserKind), claimsIdentity.FindFirst("UserKind").Value);
+			}
+			catch
+			{
+				return request.CreateErrorResponse(HttpStatusCode.BadRequest, "Malformed Token");
+			}
+			var userId = Int32.Parse(claimsIdentity.FindFirst("UserId").Value);
+
+			using(var db = new TccSettings())
+				SetPrincipal(new UserPrincipal(db.tblLeitor.First(l => l.IDLeitor == userId)));
+
+
+
+			return Next(_);
+		}
+
+
+		private void SetPrincipal(IPrincipal principal)
+		{
+			Thread.CurrentPrincipal = principal;
+			if (HttpContext.Current != null)
+			{
+				HttpContext.Current.User = principal;
+			}
+		}
+
+		private HttpResponseMessage Next((HttpRequestMessage request, CancellationToken cancellationToken) _)
+		{
+			return base.SendAsync(_.request, _.cancellationToken).GetAwaiter().GetResult();
+		}
+	}
+
+}
