f2e9077e0e06cb3ef4decc3a08c9919e0c8ee4e1
savioacp <savioacp@gmail.com>
2020-10-18 17:32:08 -0300

    Update ProdutosController.cs
    Adicionado pesquisa no produto

--- Bookworm API/Controllers/ProdutosController.cs
+++ Bookworm API/Controllers/ProdutosController.cs
@@ -52,6 +52,50 @@ namespace Bookworm_API.Controllers
                     produtos,
                 } as object);
             }
-		}
+        }
+
+        public JsonResult<object> Get(string query, int page = 1, int results = 20)
+        {
+            using (var db = new TccSettings())
+            {
+                int prodCount = db.tblProduto.Count();
+                if (page * results > prodCount)
+                    return Json(new
+                    {
+                        total_count = prodCount,
+                        count = 0,
+                        produtos = new object[] { }
+                    } as object);
+
+                var produtos = db.tblProduto
+                    .Where(p => p.Editora.Contains(query) || p.NomeLivro.Contains(query) || p.AutoresLivro.Contains(query))
+                    .OrderByDescending(p => p.IDProduto)
+                    .Skip(page * results)
+                    .Take(results)
+                    .Select(p => new {
+                        p.AnoEdicao,
+                        p.AutoresLivro,
+                        p.DescricaoProd,
+                        p.Editora,
+                        p.Fileira,
+                        p.IDProduto,
+                        p.ImagemProd,
+                        p.NomeLivro,
+                        p.Prateleira,
+                        p.Setor,
+                        Reservas = p.tblReserva.OrderBy(r => r.DataReserva).ToArray(),
+                        p.TipoAcervo,
+                        p.TipoProduto,
+                    })
+                    .ToList();
+
+                return Json(new
+                {
+                    total_count = prodCount,
+                    count = produtos.Count,
+                    produtos,
+                } as object);
+            }
+        }
     }
 }
